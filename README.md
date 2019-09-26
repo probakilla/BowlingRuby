3bae01c0fe23311ea6fd6c73e66944879591be84
Ndour <PapaLaity.Ndour@fr.nestle.com>
2019-09-26 10:20:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bowling Game
+
+This si a Kata realized in Ruby in order to improve my skills in TDD, Ruby and CI.
---
+++ tests/game_test.rb
@@ -0,0 +1,7 @@
+require 'test/unit'
+
+class GameTest < Test::Unit::TestCase
+  def test_dummy
+    assert(false)
+  end
+end
